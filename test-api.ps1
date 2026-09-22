3dee899e899ee3041dd6730a49488ae77f11bc89
maximekpogli-afk <maximekpogli-afk@users.noreply.github.com>
2026-09-22 23:42:55 +0000

    chore: remove test-api.ps1

--- test-api.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "SilentlyContinue"
-$body = '{"email":"admin@epl-lumieres.local","password":"admin123","name":"Admin System"}'
-try {
-    $r = Invoke-WebRequest -Uri "http://localhost:3000/api/auth/sign-up/email" -Method POST -Body $body -ContentType "application/json" -UseBasicParsing -TimeoutSec 120
-    "SIGNUP: " + $r.StatusCode + " " + $r.Content
-} catch {
-    "SIGNUP ERR: " + $_.Exception.Message
-}
